31514c7acb9b109c28486b862110367d71410cc7
Jacob Hamrick <thejacobhamrick@gmail.com>
2020-03-10 22:34:43 -0400

    toggleGrid build. need slicker position transition.

--- myscript.js
+++ myscript.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
 
+
+  const image = $(".image-wrapper");
+
+  function pileImages() {
+    var containerWidth = $("main").height();
+    var containerHeight = $("main").width();
+    image.each( (index,img)=> {
+      var randX = Math.floor((Math.random() * containerWidth / 1));
+      var randY = Math.floor((Math.random() * containerHeight / 3));
+      console.log(randY);
+      console.log(randX);
+      $(img).css("transition", "all 1.0s")
+      $(img).css("left", randX);
+      $(img).css("top", randY);
+    });
+  }
+
+  // pile on start up
+  pileImages();
+
+
+  function toggleGrid() {
+    $(".image-row").toggleClass("active");
+    if ($(".image-row").hasClass("active")) {
+      $(image).css("transition", "all 1.0s")
+      $(image).css("top", "0");
+      $(image).css("left", "0");
+    } else {
+      pileImages();
+    }
+  };
+
   function toggleSwitch() {
     $(".about-window").toggleClass("display-switch");
   }
@@ -8,14 +40,17 @@ $(document).ready(function(){
     toggleSwitch()
   });
 
-  $("#info-button").click(function(){
+  $("#info-button").click(function() {
     toggleSwitch()
   });
 
+  $(".gridImages").click(function() {
+    toggleGrid();
+  });
 
-
-  const image = $(".image-wrapper");
-
+  $(".pileImages").click(function() {
+    pileImages();
+  });
 
   image.fancybox(
     {
@@ -40,35 +75,12 @@ $(document).ready(function(){
     }
   );
 
-  // stops fancybox from opening after image is dragged
-  document.addEventListener("dragend", function(event) {
-    $.fancybox.close();
-  });
 
-
-
-
-// to-do: get screensize and update math depending on screen-size
-
-  var containerWidth = $("main").height();
-  var containerHeight = $("main").width();
-
-  function pileImages() {
-    image.each( (index,img)=> {
-      var randX = Math.floor((Math.random() * containerWidth / 1.1));
-      var randY = Math.floor((Math.random() * containerHeight / 3));
-      console.log(randY);
-      console.log(randX);
-      $(img).css("left", randX);
-      $(img).css("top", randY);
+    // stops fancybox from opening after image is dragged
+    document.addEventListener("dragend", function(event) {
+      $.fancybox.close();
+      console.log("drag end fired")
     });
-  }
-
-  pileImages();
-
-  $(".pileImages").click(function() {
-    pileImages();
-  });
 
 
 });//end document.ready
